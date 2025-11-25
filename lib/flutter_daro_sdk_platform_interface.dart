2b20660f47ba1db29e3fd94a3fb0bef3655dbe97
huansuh <huansuh@jobis.co>
2025-11-25 10:39:03 +0900

    refactor: DaroRewardAdConfig 통합 및 adKey 필수화
    - DaroInterstitialAdConfig를 DaroRewardAdConfig로 통합
    - adKey를 필수 파라미터로 변경
    - placement 필드 추가
    - 인터스티셜, 리워드 비디오, 팝업 광고를 하나의 Config로 통합

--- lib/flutter_daro_sdk_platform_interface.dart
+++ lib/flutter_daro_sdk_platform_interface.dart
@@ -15,10 +15,10 @@ enum DaroAppCategory {
 enum DaroAdType {
   /// 전면광고
   interstitial,
-  
+
   /// 리워드 비디오 광고
   rewardedVideo,
-  
+
   /// 팝업광고
   popup,
 }
@@ -45,25 +45,20 @@ class DaroSdkConfig {
 class DaroRewardAdConfig {
   /// 광고 타입
   final DaroAdType adType;
-  
-  /// 광고 키 (선택사항)
-  final String? adKey;
-  
+
+  /// 광고 키 (필수)
+  final String adKey;
+
+  /// Placement (로그에 표시될 이름, 선택사항)
+  final String? placement;
+
   /// 추가 파라미터 (선택사항)
   final Map<String, dynamic>? extraParams;
 
-  DaroRewardAdConfig({
-    required this.adType,
-    this.adKey,
-    this.extraParams,
-  });
+  DaroRewardAdConfig({required this.adType, required this.adKey, this.placement, this.extraParams});
 
   Map<String, dynamic> toMap() {
-    return {
-      'adType': adType.name,
-      'adKey': adKey,
-      'extraParams': extraParams,
-    };
+    return {'adType': adType.name, 'adKey': adKey, 'placement': placement, 'extraParams': extraParams};
   }
 }
 
@@ -71,7 +66,7 @@ class DaroRewardAdConfig {
 class DaroAdResult {
   /// 광고 ID (이벤트 리스너 등록에 사용)
   final String adId;
-  
+
   /// 광고 표시 성공 여부
   final bool success;
 
@@ -81,12 +76,7 @@ class DaroAdResult {
   /// 리워드 적립 금액 (Reward 앱인 경우)
   final int? rewardAmount;
 
-  DaroAdResult({
-    required this.adId,
-    required this.success,
-    this.errorMessage,
-    this.rewardAmount,
-  });
+  DaroAdResult({required this.adId, required this.success, this.errorMessage, this.rewardAmount});
 
   factory DaroAdResult.fromMap(Map<dynamic, dynamic> map) {
     return DaroAdResult(
@@ -100,6 +90,45 @@ class DaroAdResult {
 
 /// 광고 이벤트 리스너
 typedef DaroAdListener = void Function(String adId, Map<dynamic, dynamic> event);
+
+/// 리워드 광고 클래스 (인터스티셜, 리워드 비디오, 팝업)
+class DaroRewardAd {
+  /// 광고 타입
+  final DaroAdType adType;
+
+  /// 광고 키
+  final String adKey;
+
+  /// 플랫폼 인터페이스
+  final FlutterDaroSdkPlatform _platform;
+
+  DaroRewardAd(this.adType, this.adKey, this._platform);
+
+  /// 광고 로드
+  Future<void> load(DaroRewardAdConfig config) async {
+    await _platform.loadRewardAd(adType, adKey, config);
+  }
+
+  /// 광고 표시
+  Future<bool> show() async {
+    return await _platform.showRewardAdInstance(adType, adKey);
+  }
+
+  /// 광고 이벤트 리스너 등록
+  void addListener(DaroAdListener listener) {
+    _platform.addRewardAdListener(adKey, listener);
+  }
+
+  /// 광고 이벤트 리스너 제거
+  void removeListener() {
+    _platform.removeRewardAdListener(adKey);
+  }
+
+  /// 광고 인스턴스 해제
+  Future<void> dispose() async {
+    await _platform.disposeRewardAd(adType, adKey);
+  }
+}
 
 abstract class FlutterDaroSdkPlatform extends PlatformInterface {
   /// Constructs a FlutterDaroSdkPlatform.
@@ -127,7 +156,7 @@ abstract class FlutterDaroSdkPlatform extends PlatformInterface {
     throw UnimplementedError('initialize() has not been implemented.');
   }
 
-  /// 리워드 광고 표시
+  /// 리워드 광고 표시 (기존 API - 호환성 유지)
   Future<DaroAdResult> showRewardAd(DaroRewardAdConfig config) {
     throw UnimplementedError('showRewardAd() has not been implemented.');
   }
@@ -140,5 +169,30 @@ abstract class FlutterDaroSdkPlatform extends PlatformInterface {
   /// 광고 이벤트 리스너 제거
   void removeAdListener(String adId) {
     throw UnimplementedError('removeAdListener() has not been implemented.');
+  }
+
+  /// 리워드 광고 로드 (인터스티셜, 리워드 비디오, 팝업)
+  Future<void> loadRewardAd(DaroAdType type, String adKey, DaroRewardAdConfig config) {
+    throw UnimplementedError('loadRewardAd() has not been implemented.');
+  }
+
+  /// 리워드 광고 인스턴스 표시 (인터스티셜, 리워드 비디오, 팝업)
+  Future<bool> showRewardAdInstance(DaroAdType type, String adKey) {
+    throw UnimplementedError('showRewardAdInstance() has not been implemented.');
+  }
+
+  /// 리워드 광고 리스너 등록
+  void addRewardAdListener(String adKey, DaroAdListener listener) {
+    throw UnimplementedError('addRewardAdListener() has not been implemented.');
+  }
+
+  /// 리워드 광고 리스너 제거
+  void removeRewardAdListener(String adKey) {
+    throw UnimplementedError('removeRewardAdListener() has not been implemented.');
+  }
+
+  /// 리워드 광고 인스턴스 해제
+  Future<void> disposeRewardAd(DaroAdType type, String adKey) {
+    throw UnimplementedError('disposeRewardAd() has not been implemented.');
   }
 }
